5d8d19d10b01b0eb7de951a00e34b6cde94d70cb
fayro <fayro@FAYROUZGOUMAA>
2026-02-21 01:19:06 +0200

    Q1

--- G-NET-42-C#-5/Program.cs
+++ G-NET-42-C#-5/Program.cs
@@ -4,7 +4,73 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            #region Q1 
+            //Console.Write("Enter first number: ");
+            //double firstNumber = double.Parse(Console.ReadLine() ?? "0");
+
+            //Console.Write("Enter second number: ");
+            //double secondNumber = double.Parse(Console.ReadLine() ?? "0");
+
+            //Console.Write("Enter operation (+, -, *, /): ");
+            //string operation = Console.ReadLine() ?? "";
+
+            //double result;
+
+            //switch (operation)
+            //{
+            //    case "+":
+            //        result = Add(firstNumber, secondNumber);
+            //        Console.WriteLine($"Result = {result}");
+            //        break;
+
+            //    case "-":
+            //        result = Subtract(firstNumber, secondNumber);
+            //        Console.WriteLine($"Result = {result}");
+            //        break;
+
+            //    case "*":
+            //        result = Multiply(firstNumber, secondNumber);
+            //        Console.WriteLine($"Result = {result}");
+            //        break;
+
+            //    case "/":
+            //        if (secondNumber == 0)
+            //        {
+            //            Console.WriteLine("Error: Division by zero is not allowed.");
+            //        }
+            //        else
+            //        {
+            //            result = Divide(firstNumber, secondNumber);
+            //            Console.WriteLine($"Result = {result}");
+            //        }
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Invalid operation selected.");
+            //        break;
+            //}
+                #endregion
+            }
+        #region Q1
+        //static double Add(double number1, double number2)
+        //{
+        //    return number1 + number2;
+        //}
+
+        //static double Subtract(double number1, double number2)
+        //{
+        //    return number1 - number2;
+        //}
+
+        //static double Multiply(double number1, double number2)
+        //{
+        //    return number1 * number2;
+        //}
+
+        //static double Divide(double number1, double number2)
+        //{
+        //    return number1 / number2;
+        //}
+#endregion
     }
 }
